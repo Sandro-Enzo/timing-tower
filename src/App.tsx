745c25ff69095ac2e1d1b6219a87dbb13357428d
Sandro de Gruijter <sandrodegruijter@gmail.com>
2023-02-27 22:30:44 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,82 @@
+import './App.css';
+import Driver from './Driver';
+
+import drivers from './drivers.json';
+import { useEffect } from 'react';
+
+function App() {
+    useEffect(() => {
+        document.documentElement.style.setProperty(
+            '--drivers',
+            `${drivers.drivers.length}`
+        );
+    });
+
+    let gap = 0;
+
+    return (
+        <>
+            <div className='tower-container'>
+                <div className={`blurry-background `}>
+                    <div className='main-background'>
+                        <div className='header'>
+                            <div className='title'></div>
+                            <div className='race-info'>
+                                <div className='laps'>
+                                    <span>
+                                        LAP{' '}
+                                        <span
+                                            style={{
+                                                color: 'white',
+                                                fontSize: '1.5em',
+                                            }}
+                                        >
+                                            3
+                                        </span>
+                                        /57
+                                    </span>
+                                </div>
+                                <div className='checkpoints'>
+                                    <span>
+                                        CP{' '}
+                                        <span
+                                            style={{
+                                                color: 'white',
+                                                fontSize: '1.5em',
+                                            }}
+                                        >
+                                            8
+                                        </span>
+                                        /46
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='flag'>Blue Flag</div>
+                        <div className='drivers'>
+                            {drivers.drivers.map((driver, index) => {
+                                const output = (
+                                    <Driver
+                                        brand={driver.brand}
+                                        gap={gap.toFixed(3)}
+                                        name={driver.name}
+                                        position={driver.position}
+                                        player={'player' in driver}
+                                        key={index}
+                                    />
+                                );
+
+                                gap += Math.random() * 3;
+
+                                return output;
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
+
---
+++ src/Driver.tsx
@@ -0,0 +1,32 @@
+import './Driver.css';
+
+interface IDriver {
+    position: number;
+    brand: string;
+    name: string;
+    gap: string;
+    player: boolean;
+}
+
+function Driver({ position, brand, name, gap, player }: IDriver) {
+    const displayGap = parseFloat(gap) > 0 ? `+${gap}` : `-${gap}`;
+
+    return (
+        <div className={`driver ${player ? 'player' : ''}`}>
+            <div className='position'>{position}</div>
+            <div
+                className='brand'
+                style={{
+                    backgroundImage: `url('../public/images/${brand}.webp')`,
+                }}
+            ></div>
+            <div className={`name`}>{name}</div>
+            <div className='gap'>
+                {parseFloat(gap) > 0 ? displayGap : 'Leader'}
+            </div>
+            {/* <div className="tire">S</div> */}
+        </div>
+    );
+}
+
+export default Driver;
